8f1e48686150470fdb9b1866dc25156c35b96596
Malak Zaidi <malakzaidi815@gmail.com>
2025-01-05 20:46:36 +0100

    Initial commits of the exam project

---
+++ src/main/java/com/javafx/test/App.java
@@ -0,0 +1,23 @@
+package com.javafx.test;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage primaryStage){ }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
